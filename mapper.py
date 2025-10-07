9b3cb07f380423668d930f6d05d460b681c06af9
MaramRebai8 <maram.rebai@supcom.tn>
2025-10-07 13:37:43 +0200

    Add mapper.py file

---
+++ mapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    if len(line)==0:
+        continue
+    
+    words = line.split()
+    for word in words:
+        print (word,"\t",1)
