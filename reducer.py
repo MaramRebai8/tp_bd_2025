0c5cbeef2fdc8d6a402c9fa7ae5d2e1f32a5ca1d
MaramRebai8 <maram.rebai@supcom.tn>
2025-10-07 13:47:34 +0200

    Add mapper.py file

---
+++ reducer.py
@@ -0,0 +1,17 @@
+import sys
+Total = 0
+oldKey = None
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        continue
+    thisKey, thisNb = data_mapped
+    if oldKey and oldKey != thisKey:
+        print (oldKey, "\t", Total)
+        oldKey = thisKey
+        Total = 0
+    oldKey = thisKey
+    Total += int(thisNb)
+
+if oldKey != None:
+    print (oldKey, "\t", Total)
